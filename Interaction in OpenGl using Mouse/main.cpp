d3fb20b032c40fef31ccde4666359d0084b75107
jennansowayan <42040549+jennansowayan@users.noreply.github.com>
2020-03-24 17:45:37 +0300

    added func.s

--- Interaction in OpenGl using Mouse/main.cpp
+++ Interaction in OpenGl using Mouse/main.cpp
@@ -12,15 +12,36 @@
 #include <GLUT/glut.h>
 #include <stdlib.h>
 
+int width = 600, height = 400;
+int first = 0;
+int xi, yi, xf, yf;
 
+void draw(void){}
+
+void drawLine(){
+    
+}
+
+void mouse(int button, int state, int x, int y){
+    
+    switch (button) {
+        case GLUT_LEFT_BUTTON:
+            if(state == GLUT_DOWN)
+                
+            break;
+            
+        default:
+            break;
+    }
+}
 
 int main(int argc, char** argv){
     glutInit(&argc, argv);
     glutInitWindowPosition(100, 100);
-    glutInitWindowSize(600, 600);
-    glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH);
-    glutCreateWindow("Triangles");
-    glOrtho(-100.0, 100.0, -100.0, 100.0, -100.0, 100.0);
+    glutInitWindowSize(width, height);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutCreateWindow("Project");
+    gluOrtho2D(0, width, 0, height);
     glutMainLoop();
 
 }
